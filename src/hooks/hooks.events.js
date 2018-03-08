24f8fe61979074fb83ea6817690e79bf7a9ad334
Luc Claustres <luc.claustres@orange.fr>
2018-03-08 17:37:13 +0100

    WIP https://github.com/kalisio/kdk/issues/6 (event notifications)

--- src/hooks/hooks.events.js
+++ src/hooks/hooks.events.js
@@ -1,6 +1,19 @@
 import logger from 'winston'
+import _ from 'lodash'
 import makeDebug from 'debug'
 const debug = makeDebug('kalisio:kEvent:events:hooks')
+
+export function processNotification (hook) {
+  // We use a query parameter to transport the notification body when creating, updating, removing an event
+  // Indeed it needs to be translated so it is sent by the client depending on its locale
+  const notification = _.get(hook.params, 'query.notification')
+  if (notification) {
+    hook.params.notification = notification
+    // Delete from query otherwise it will be used to filter items
+    _.unset(hook.params, 'query.notification')
+  }
+  return hook
+}
 
 export function addCreatorAsCoordinator (hook) {
   if (hook.type !== 'before') {
@@ -24,50 +37,49 @@ export function addCreatorAsCoordinator (hook) {
   return hook
 }
 
-export function sendNotifications (body) {
-  return async function (hook) {
-    if (hook.type !== 'after') {
-      throw new Error(`The 'sendNotifications' hook should only be used as a 'after' hook.`)
-    }
-
-    let pusherService = hook.app.getService('pusher')
-    if (!pusherService) return hook
-    const participants = hook.result.participants || []
-    let publishPromises = []
-    participants.forEach(participant => {
-      let participantService = participant.service
-      if (hook.service.context) {
-        participantService = (typeof hook.service.context === 'object' ? hook.service.context._id.toString() : hook.service.context) + '/' + participantService
-      }
-      publishPromises.push(pusherService.create({
-        action: 'message',
-        // The notification contains the event title + a given prefix
-        message: {
-          title: hook.result.name,
-          body,
-          createdAt: hook.result.createdAt,
-          updatedAt: hook.result.updatedAt,
-          // Custom vibration pattern
-          vibration: [500, 1000, 500, 500, 500, 500],
-          sound: 'default'
-        },
-        pushObject: participant._id.toString(),
-        pushObjectService: participantService
-      }))
-    })
-    // We'd like to be tolerant here because the participants might have be removed from the system while the event is still alive
-    //let results = await Promise.all(publishPromises)
-    let results = []
-    for (let i = 0; i < publishPromises.length; i++) {
-      try {
-        let result = await publishPromises[i]
-        results.push(result)
-      } catch (error) {
-        logger.error(error.message, error)
-      }
-    }
-    debug('Published event notifications on ' + results.length + ' topics/users for event ' + hook.result._id.toString())
-    return hook
+export async function sendEventNotifications (hook) {
+  if (hook.type !== 'after') {
+    throw new Error(`The 'sendNotifications' hook should only be used as a 'after' hook.`)
   }
+
+  let pusherService = hook.app.getService('pusher')
+  if (!pusherService) return hook
+  const notification = hook.params.notification || ''
+  const participants = hook.result.participants || []
+  let publishPromises = []
+  participants.forEach(participant => {
+    let participantService = participant.service
+    if (hook.service.context) {
+      participantService = (typeof hook.service.context === 'object' ? hook.service.context._id.toString() : hook.service.context) + '/' + participantService
+    }
+    publishPromises.push(pusherService.create({
+      action: 'message',
+      // The notification contains the event title + a given prefix
+      message: {
+        title: hook.result.name,
+        body: notification,
+        createdAt: hook.result.createdAt,
+        updatedAt: hook.result.updatedAt,
+        // Custom vibration pattern
+        vibration: [500, 1000, 500, 500, 500, 500],
+        sound: 'default'
+      },
+      pushObject: participant._id.toString(),
+      pushObjectService: participantService
+    }))
+  })
+  // We'd like to be tolerant here because the participants might have be removed from the system while the event is still alive
+  //let results = await Promise.all(publishPromises)
+  let results = []
+  for (let i = 0; i < publishPromises.length; i++) {
+    try {
+      let result = await publishPromises[i]
+      results.push(result)
+    } catch (error) {
+      logger.error(error.message, error)
+    }
+  }
+  debug('Published event notifications on ' + results.length + ' topics/users for event ' + hook.result._id.toString())
+  return hook
 }
 
