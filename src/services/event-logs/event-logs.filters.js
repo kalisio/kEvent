9d059374dba34be3d55911d9597c1d8e4c60eb7e
Luc Claustres <luc.claustres@orange.fr>
2018-07-18 21:03:56 +0200

    Removed unused filter files

--- src/services/event-logs/event-logs.filters.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (data, connection, hook) { // eslint-disable-line no-unused-vars
-  // Clients are only interested in create log events, others are used internally
-  return (hook.method !== 'create' ? false : data)
-}
