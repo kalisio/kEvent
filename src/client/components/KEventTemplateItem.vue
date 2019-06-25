6cd299c235d71e956a438d79c66d77cad8dab00c
Luc Claustres <luc.claustres@orange.fr>
2019-06-25 20:07:27 +0200

    Added event template item to be used in list

---
+++ src/client/components/KEventTemplateItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <k-item v-bind="$props" :itemActions="actions.pane" @item-selected="onItemSelected">
+    <q-item-side slot="item-icon" :icon="icon.name" :color="icon.color"></q-item-side>
+  </k-item>
+</template>
+
+<script>
+import { mixins } from '@kalisio/kdk-core/client'
+import { QItemSide } from 'quasar'
+
+export default {
+  name: 'k-event-template-item',
+  mixins: [ mixins.baseItem ],
+  components: {
+    QItemSide
+  },
+  computed: {
+    icon () {
+      return this.item.icon
+    }
+  },
+  created () {
+    // Load the required components
+    this.$options.components['k-item'] = this.$load('collection/KItem')
+  }
+}
+</script>
