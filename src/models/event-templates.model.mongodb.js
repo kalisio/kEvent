864cd59ffd6c9782ae307be6216a91834e58a743
Luc Claustres <luc.claustres@orange.fr>
2020-02-17 20:51:58 +0100

    closes https://github.com/kalisio/kEvent/issues/65

--- src/models/event-templates.model.mongodb.js
+++ src/models/event-templates.model.mongodb.js
@@ -5,4 +5,7 @@ module.exports = function (app, options) {
   // We built indices with collation to cover the most used languages, it requires different naming...
   options.Model.createIndex({ name: 1 }, { name: 'name-en', collation: { locale: 'en', strength: 1 } })
   options.Model.createIndex({ name: 1 }, { name: 'name-fr', collation: { locale: 'fr', strength: 1 } })
+  // Full text search is used by event archiving to perform template matching
+  options.Model.createIndex({ name: 'text' })
+  options.Model.createIndex({ description: 'text' })
 }
