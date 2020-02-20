f1f7ac3baa11db64da85be833354892bf9a57268
Luc Claustres <luc.claustres@orange.fr>
2020-02-20 14:34:07 +0100

    Fixed lint

--- src/client/components/KArchivedEventEntry.vue
+++ src/client/components/KArchivedEventEntry.vue
@@ -19,7 +19,6 @@
 </template>
 
 <script>
-import _ from 'lodash'
 import { mixins as kCoreMixins } from '@kalisio/kdk-core/client'
 
 export default {
--- src/client/components/KArchivedEventsActivity.vue
+++ src/client/components/KArchivedEventsActivity.vue
@@ -48,6 +48,7 @@
 </template>
 
 <script>
+import _ from 'lodash'
 import moment from 'moment'
 import { mixins as kCoreMixins, utils as kCoreUtils } from '@kalisio/kdk-core/client'
 import { mixins as kMapMixins } from '@kalisio/kdk-map/client.map'
